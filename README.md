205a63a3ebd2fba2ae0df27a4f737b4fb2900bed
Alina Solovyeva <akilina.vsk@gmail.com>
2022-05-14 10:23:45 +0300

    fixed step 6

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 <a href="https://codeclimate.com/github/aki1ina/python-project-lvl1-1/maintainability"><img src="https://api.codeclimate.com/v1/badges/49cfd3ad26dd3cb7e2ec/maintainability" /></a>
 
 brain-even <a href="https://asciinema.org/a/3BqmiYcTUvijO39T8zAiyilnc" target="_blank"><img src="https://asciinema.org/a/3BqmiYcTUvijO39T8zAiyilnc.svg" /></a>
+
+brain-calc <a href="https://asciinema.org/a/uoaaRDpJs1BQ4JNkIlZsQact9" target="_blank"><img src="https://asciinema.org/a/uoaaRDpJs1BQ4JNkIlZsQact9.svg" /></a>
---
+++ brain_games/games/calc.py
@@ -0,0 +1,17 @@
+import random
+
+
+rules = 'What is the result of the expression?'
+
+def question_answer():
+    number1 = random.randint(0, 150)
+    number2 = random.randint(0, 150)
+    action = random.choice([' + ', ' - ', ' * '])
+    if action == ' + ':
+        answer = str(number1 + number2)
+    elif action == ' - ':
+        answer = str(number1 - number2)
+    elif action == ' * ':
+        answer = str(number1 * number2)
+    question = str(number1) + action + str(number2)
+    return question, answer
