0abfac602de7c852c6516e06258f14cece745810
Alina Solovyeva <akilina.vsk@gmail.com>
2022-05-15 12:09:16 +0300

    fixed file progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,16 +5,15 @@ rules = 'What number is missing in the progression?'
 
 def question_answer():
     number = random.randint(1, 100)
-    question = ''
+    question = str(number)
     step = random.randint(1, 10)
-    hidden = random.randint(1, 10)
+    hidden = random.randint(2, 10)
+    answer = str(number + step * hidden)
     i = 1
-    while i <= 10:
+    while i < 10:
         if i == hidden:
-            answer = str(number)
             question += ' ..'
         else:
-            question += ' ' + str(number)
+            question += ' ' + str(number + step * i)
         i += 1
-        number += step
     return question, answer
