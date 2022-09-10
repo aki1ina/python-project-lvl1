fe057af5332de8b7420ecc5d13b88968b2f1472d
Alina Solovyeva <akilina.vsk@gmail.com>
2022-09-10 23:33:10 +0300

    fixed progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,18 +4,23 @@ import random
 rules = 'What number is missing in the progression?'
 
 
+def get_progression(initial_term, difference, length):
+    last_term = initial_term + (length * difference)
+    progression = list(range(initial_term, last_term, difference))
+    return progression
+
+
+def convert_to_string(progression):
+    progression_string = ' '.join(map(str, progression))
+    return progression_string
+
+
 def get_question_and_answer():
-    number = random.randint(1, 100)
-    question = ''
-    step = random.randint(1, 10)
+    initial_term = random.randint(1, 100)
+    difference = random.randint(1, 10)
     hidden = random.randint(2, 10)
-    i = 1
-    while i <= 10:
-        if i == hidden:
-            question += '.. '
-            answer = str(number)
-        else:
-            question += str(number) + ' '
-        i += 1
-        number += step
-    return question, answer
+    length = 10
+    progression = get_progression(initial_term, difference, length)
+    answer, progression[hidden] = progression[hidden], '..'
+    question = convert_to_string(progression)
+    return question, str(answer)
