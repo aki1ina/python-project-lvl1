d4b1f4912abfe6c990f0ddfc82d284ff63308288
Alina Solovyeva <akilina.vsk@gmail.com>
2022-09-17 18:00:52 +0300

    fixed prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,19 +5,12 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(question):
-    check = 0
-    divider = 2
     if question < 2:
         return False
-    else:
-        while divider <= question // 2:
-            if question % divider == 0:
-                check += 1
-                divider = question
-            divider += 1
-        if check != 0:
+    for i in range(2, question):
+        if question % i == 0:
             return False
-    return True
+        return True
 
 
 def get_question_and_answer():
