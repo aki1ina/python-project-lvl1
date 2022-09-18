20cb280656582d6039bc34ec093860609d8c947d
Alina Solovyeva <akilina.vsk@gmail.com>
2022-09-18 15:45:34 +0300

    fixed even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(question):
-    return question % 2 = 0
+    return question % 2 == 0
 
 
 def get_question_and_answer():
